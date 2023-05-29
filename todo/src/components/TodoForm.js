7009c1aa92c2d36903faf6b234731075e3443d27
Kauan Correia <kcorreia925@gmail.com>
2023-05-29 11:49:34 -0300

    Primeiro ReactApp

---
+++ todo/src/components/TodoForm.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+const TodoForm = ({addTodo}) => {
+  
+    const [title, setTitle] = useState("")
+    const [category, setCategory] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(!title || !category) return;
+        addTodo(title, category)
+        setCategory("")
+        setTitle("")
+    }
+
+    return (
+    <div className='todo-form'>
+        <h2>Criar tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='Digite o titulo' onChange={(e) => setTitle(e.target.value)} value={title}/>
+            <select onChange={(e) => setCategory(e.target.value)} value={category}>
+                <option value="">Selecione uma categoria</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Pessoal">Pessoal</option>
+                <option value="Estudos">Estudos</option>
+            </select>
+            <button type='submit'>Criar tarefa:</button>
+        </form>
+
+    </div>  
+  )
+}
+
+export default TodoForm
